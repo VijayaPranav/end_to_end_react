117718668a157d35c7c0b9464f31d9ff8682d627
VijayaPranav <vijayapranav53@gmail.com>
2026-08-27 21:08:26 +0530

    CI pipeline checking commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <h1>React-App build</h1>
       <p><strong>Title:</strong> {process.env.REACT_APP_TITLE}</p>
       <p><strong>Version:</strong> {process.env.REACT_APP_VERSION}</p>
-      <p>Follows a complete DevOps end-to-end pipeline</p>
+      <p>Follows a end-to-end DevOps pipeline</p>
     </div>
   );
 }
